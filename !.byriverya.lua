9c939fc8bbcda253e5d8eb4b0f67d7ba05a176ef
riverya4life <111198704+riverya4life@users.noreply.github.com>
2023-03-21 02:15:01 +0200

    Неактуально (используйте samp.lua)

--- !.byriverya.lua
+++ !.byriverya.lua
@@ -1,6 +1,5 @@
 script_name("!.byriverya")
 script_version("0.8")
 script_author('RIVERYA4LIFE.')
-require 'lib.moonloader'
 
---[Скрипт пока обновляется]
+--[Скрипт устарел, используйте новый samp.lua]
